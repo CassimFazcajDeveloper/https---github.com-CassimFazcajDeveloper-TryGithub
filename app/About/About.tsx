00a55d9cd106c0413e166162b981e6b6986bba39
AshrafChitamba <bsc-40-20@unima.ac.mw>
2026-04-06 12:16:31 +0200

    updated

--- app/About/About.tsx
+++ app/About/About.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <h1>About page updated</h1>
+      <h1>About page updated</h1>
+      <h1>About page updated</h1>
     </div>
-  )
+  );
 }
 
 export default About
